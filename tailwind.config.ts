acbb3daef31d6f1ab15ada37424317a4f62d3ef1
Stoic Coder <110672923+ShubhMehrotra19@users.noreply.github.com>
2024-11-13 00:01:59 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        scroll: '20s linear infinite scroll',
+        scroll1: '20s linear infinite scroll1', // New animation
+      },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' }
+        },
+        scroll1: { // New keyframe
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(-50%)' }
+        }
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
