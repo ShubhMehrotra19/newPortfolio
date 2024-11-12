12a5048e75575a30a51d8566b359a3d807e1fd86
Stoic Coder <110672923+ShubhMehrotra19@users.noreply.github.com>
2024-11-13 00:02:26 +0530

    Update marqueeMix.tsx

--- components/marqueeMixed/marqueeMix.tsx
+++ components/marqueeMixed/marqueeMix.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Marquee from './marquee/marquee'
-import Marquee2 from './marquee2/marquee2'
+import React from "react";
+import Marquee from "./marquee/marquee";
+import Marquee2 from "./marquee2/marquee2";
 
 interface Props {}
 
 function MarqueeMix(props: Props) {
-    const {} = props
+  const {} = props;
 
-    return (
-        <section className='md:mb-64 mb-24 relative w-full'>
-                <Marquee />
-                <Marquee2 />
-        </section>
-    )
+  return (
+    <section className="md:mb-64 mb-24 relative w-screen flex flex-col justify-center items-center">
+      <Marquee />
+      <Marquee2 />
+    </section>
+  );
 }
 
-export default MarqueeMix
+export default MarqueeMix;
